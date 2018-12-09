2515c8f563cf0a6e8bab7d2148baa7da81c97c52
Javier Collazos <jcollazosgomez@gmail.com>
2018-12-08 22:58:27 -0500

    primero movimientos del snake

--- js/game.js
+++ js/game.js
@@ -14,7 +14,26 @@
     class Snake {
         constructor() {
             this.head = new Square(100,0)
+        }
+
+        draw(){
             this.head.draw();
+        }
+
+        right() {
+            this.head.x += 10;
+        }
+
+        left() {
+            this.head.x -= 10;
+        }
+
+        up() {
+            this.head.y -=10;
+        }
+
+        down() {
+            this.head.y += 10;
         }
 
     }
@@ -23,5 +42,11 @@
     const ctx = canvas.getContext('2d')
 
     const snake = new Snake()
+
+    setInterval(function(){
+        snake.right()
+        ctx.clearRect(0,0,canvas.width,canvas.height)
+        snake.draw()
+    }, 1000/10)
 
 })()
