c2c293e55396937cb4b85ea3ba1dad42c2b20d2b
Javier Collazos <jcollazosgomez@gmail.com>
2018-12-08 22:48:42 -0500

    se implementa la clase snake y square y se pinta sobre el canvas

--- js/game.js
+++ js/game.js
@@ -1,8 +1,27 @@
 ;(function() {
 
+    class Square {
+        constructor(x,y) {
+            this.x = x
+            this.y = y
+        }
+
+        draw() {
+            ctx.fillRect(this.x,this.y,10,10)
+        }
+    }
+
+    class Snake {
+        constructor() {
+            this.head = new Square(100,0)
+            this.head.draw();
+        }
+
+    }
+
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
 
-    ctx.fillRect(0,0,50,50)
+    const snake = new Snake()
 
 })()
